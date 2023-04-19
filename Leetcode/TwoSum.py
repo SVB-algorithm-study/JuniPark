4a358b3c4d90af3d0805266e3c7fd9e90b76d333
juni5184 <juni5184@naver.com>
2023-04-19 17:35:43 +0900

    2023.04.14 Solved

---
+++ Leetcode/TwoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i, num in enumerate(nums) :
+            for j, num2 in enumerate(nums):
+                if i != j and num + num2 == target :
+                    return [i,j]
