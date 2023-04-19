c9c895f623400bf445941a03ec5be034f22dff85
juni5184 <juni5184@naver.com>
2023-04-19 18:08:58 +0900

    2023.04.19 camelcase4 modified

--- HackerRank/CamelCase4/CamelCase4.py
+++ HackerRank/CamelCase4/CamelCase4.py
@@ -1,15 +1,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import sys
 
-str_list = sys.stdin.readlines()
-# print(str_list)
+input_list = sys.stdin.readlines()
+
+# 공백제거해줘야함!!!!!!!!!!!!!!!!!!!!!!!!!
+str_list = []
+for strs in input_list:
+    str_list.append(strs.rstrip())
 
 for str_input in str_list :
     wtd = str_input[0] # what to do S/C
     s_type = str_input[2] #str type M/C/V
     title = str_input[4:]
     answer = ""
-
+    
     if wtd == "S":
         # Split
         answer += title[0].lower()
@@ -19,8 +23,8 @@ for str_input in str_list :
             else :
                 answer += i
         if "()" in answer :
-            answer = answer[:-3]+"\n"
-
+            answer = answer[:-2]
+                
     elif wtd == "C":
         # Combine
         prev = False
@@ -33,11 +37,13 @@ for str_input in str_list :
                 answer += i.upper()
             else :
                 answer += i
-
-        if s_type == "M" :
-            answer = answer[:-1]+"()\n"
+            
+        if s_type == "M" : 
+            answer += "()"
         elif s_type == "C" :
             answer = answer[0].upper()+answer[1:]
+                
 
-
-    sys.stdout.write(answer)
+    sys.stdout.write(answer+"\n")
+        
+        
