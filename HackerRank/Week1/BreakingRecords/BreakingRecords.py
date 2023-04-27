f0d4eed70bf57eb634a92f321ef53f12fd0d6f98
juni5184 <juni5184@naver.com>
2023-04-28 04:31:13 +0900

    Breaking the Records update

--- HackerRank/Week1/BreakingRecords/BreakingRecords.py
+++ HackerRank/Week1/BreakingRecords/BreakingRecords.py
@@ -1,3 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
 def breakingRecords(scores):
     # Write your code here
     max_sc, min_sc = scores[0], scores[0]
@@ -12,3 +27,17 @@ def breakingRecords(scores):
             min_ans +=1       
     
     return max_ans, min_ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
